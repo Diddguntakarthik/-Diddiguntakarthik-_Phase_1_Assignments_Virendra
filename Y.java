66187a896eaef2a441480b43a193e069f95d6fcd
Diddigunta karthik <97450177+Diddguntakarthik@users.noreply.github.com>
2022-01-12 12:32:41 +0530

    Add files via upload

---
+++ Y.java
@@ -0,0 +1,12 @@
+package accessmodifier;
+import accessmodifier.N;
+
+
+	public class Y extends N{
+		public static void main(String args[]) {
+			 new N().methodPublic();
+		
+		}
+
+
+}
---
+++ Z.java
@@ -0,0 +1,9 @@
+package accessmodifier;
+
+public class Z {
+	public static void main(String args[]) {
+		new M().methodPublic();
+}
+}
+
+
